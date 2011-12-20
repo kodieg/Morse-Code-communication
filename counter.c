2e503a3b35018089678af8492f2701a09a49d8f8
Konrad Gołuchowski <kodie@mimuw.edu.pl>
2011-12-20 17:34:00 +0100

    counter

---
+++ counter.c
@@ -0,0 +1,6 @@
+/*
+ * counter.c
+ *
+ * Created: 2011-12-20 17:33:03
+ *  Author: kodie
+ */ 
---
+++ counter.h
@@ -0,0 +1,18 @@
+/*
+ * counter.h
+ *
+ * Created: 2011-12-20 17:31:30
+ *  Author: kodie
+ */ 
+
+
+#ifndef COUNTER_H_
+#define COUNTER_H_
+
+// Signal handler
+// ISR(TIMER0_OVF_vect) {
+// }
+
+void initCounter(void);
+
+#endif /* COUNTER_H_ */
