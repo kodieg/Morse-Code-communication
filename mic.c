582f7401a84235ba6d76818d22559b5bf602583f
Piotr Nowojski <piotr.nowojski@gmail.com>
2011-12-20 17:40:08 +0100

    initial changes mic for interrupts

--- mic.c
+++ mic.c
@@ -14,11 +14,11 @@ void micInit(void)
 	ADMUX = 0xC0 | MIC_PA_NUMBER;
 	// Starts ADC, starts conversion, no interrupts,
 	
-	
-	ADCSRA = 1<<ADEN | 1<<ADSC | 1<<ADATE | 1<<ADIF | 0<<ADIE |
+	ADCSRA = 1<<ADEN | 0<<ADSC | 1<<ADATE | 0<<ADIF | 1<<ADIE |
 	         1<<ADPS2 | 1<<ADPS1 | 1<<ADPS0;
 	
-	SFIOR &= 0x1F;
+	// wyzwalaj konwersje przez przepelnienie licznika 0
+	SFIOR &= 0x80;
 }
 
 uint16_t micRead(void)
